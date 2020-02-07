e6752676a244f3a91c0278410ef2bc6e61182e57
MaddyTing <37033263+MaddyTing@users.noreply.github.com>
2020-02-06 18:51:56 -0800

    Add files via upload

---
+++ jj.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class jj here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class jj
+{
+    public static void Main(String[] args)
+    {System.out.print("cat ");
+
+        System.out.println("dog ");
+
+        System.out.println("horse ");
+
+        System.out.print("cow ");
+    }
+}
